afe1d1f27ca5143e7b19f01b8db2a385dec2e846
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2023-08-08 11:23:31 +0200

    Integrate rocket and websocket in the app and remove all code

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,30 @@
-mod communication;
-mod protocol;
+#[macro_use] extern crate rocket;
 
-use std::io;
-use std::net::SocketAddr;
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::{spawn, time};
-use crate::communication::{connect_to_slave_service, start_slave_service};
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::accept;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let server = true;
+use api::index;
 
-    if server {
-        let listener = TcpListener::bind("127.0.0.1:4646").await.unwrap();
-        loop {
-            let (mut socket, _) = listener.accept().await?;
+mod api;
 
-            spawn(async move {
-                let mut buf = vec![0; 1024];
-
+#[launch]
+fn rocket() -> _ {
+    spawn(|| {
+        let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+        for stream in server.incoming() {
+            spawn(move || {
+                let mut websocket = accept(stream.unwrap()).unwrap();
                 loop {
-                    let n = socket.read(&mut buf).await.unwrap();
+                    let msg = websocket.read().unwrap();
 
-                    if n == 0 {
-                        return;
+                    // We do not want to send back ping/pong messages.
+                    if msg.is_binary() || msg.is_text() {
+                        websocket.send(msg).unwrap();
                     }
-
-                    socket.write_all(&buf[0..n]).await.unwrap();
                 }
             });
         }
-    } else {
-        // Start both master and slave services on separate tasks.
-        let slave_task = spawn(start_slave_service("0.0.0.0:5000".parse().unwrap()));
-
-        // Wait for a while to make sure the slave service is up and running.
-        //time::delay_for(std::time::Duration::from_secs(1)).await;
-
-        // Connect to the slave service and send a message.
-        if let Err(e) = connect_to_slave_service("127.0.0.1:5000".parse().unwrap(), "Hello, Slave!").await {
-            println!("Error connecting to the slave: {:?}", e);
-        }
-
-        // Wait for the slave task to finish before exiting the program.
-        if let Err(e) = slave_task.await {
-            println!("Error running slave service: {:?}", e);
-        }
-    }
-    Ok(())
+    });
+    rocket::build().mount("/", routes![index])
 }
