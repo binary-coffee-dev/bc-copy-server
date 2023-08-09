a5b6ffa49009e2c58d9cc38a2a1e9adba35f859c
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2023-08-09 18:16:24 +0200

    Integrate tera templates and create index page

--- src/api/api.rs
+++ src/api/api.rs
@@ -1,4 +1,16 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Client {
+    name: String,
+}
+
 #[get("/api/clients")]
 pub fn client_api() -> &'static str {
     "Hello, world!"
 }
+
+// #[get("/api/clients")]
+// pub fn client_api() -> &'static str {
+//     "Hello, world!"
+// }
