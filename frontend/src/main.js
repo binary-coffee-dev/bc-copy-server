b5fdfdb32e43068ae1fd40069b47957d942f80d3
Guillermo Gonzalez <guillermo.gonzalez.ext@syrocon.de>
2023-08-10 02:13:31 +0200

    Pull list of client and show them in the frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,19 @@
 import $ from "jquery";
 import {env} from "./env/env";
 
-$.ajax({
-    url: `${env.api}/clients`,
-    data: {},
-    success: function( result ) {
-        console.log(result);
-    }
-});
+function pullClients() {
+    $.ajax({
+        url: `${env.api}/clients`,
+        data: {},
+        success: function (result) {
+            const clients = JSON.parse(result);
+            // console.log(clients);
+            $('#clientList').empty();
+            for (let i = 0; i < clients.length; i++) {
+                $('#clientList').append(`<tr><td>${clients[i].name}</td></tr>`);
+            }
+        }
+    });
+}
+
+setInterval(pullClients, 1000);
